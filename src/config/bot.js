3633c55e8a93941a0943ff58c0eeb13a448472cb
joshmajesticrp-commits <josh.majesticrp@gmail.com>
2026-06-04 01:22:01 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "xxxTentaction",
         // Activity type number (0 = Playing).
-        type: 2, 
+        type: Listening, 
       },
     ],
   },
