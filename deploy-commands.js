6d05aa63300d3efd860a73a8f31b093346e70053
joshmajesticrp-commits <josh.majesticrp@gmail.com>
2026-06-10 20:38:20 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const { REST, Routes } = require('discord.js');
 
 const commands = [
@@ -7,19 +9,22 @@ const commands = [
   },
 ];
 
-const rest = new REST({ version: '10' }).setToken('');
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
     console.log('Slash Commands werden registriert...');
 
     await rest.put(
-      Routes.applicationGuildCommands('1512960643219849347', '1512178191413018824'),
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
       { body: commands }
     );
 
-    console.log('Fertig!');
+    console.log('✔ Slash Commands erfolgreich registriert!');
   } catch (error) {
-    console.error(error);
+    console.error('❌ Fehler beim Registrieren:', error);
   }
 })();
